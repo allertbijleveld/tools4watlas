6032b89b03e3a83fcd552b2968784948a3205616
Johannes Krietsch <johannes.krietsch@nioz.nl>
2024-11-26 19:46:38 +0100

    updated function to deal with maximally 6 digits tag numbers and added checks

--- R/fun_full_tag_id.R
+++ R/fun_full_tag_id.R
@@ -1,29 +1,35 @@
-#' Create full tag name
-#' 
-#' @importFrom stringr str_pad
+#' Create full tag ID or tag ID with specific length
 #'
-#' @param tag tag ID, either numeric or character
+#' @author Johannes Krietsch
+#' @param tag Tag number (either numeric or character). Maximally provide 6
+#' digits, but less work.
 #' @param short TRUE or FALSE for short or long tag ID
+#' @param n if short = TRUE, how many digits should the short tag ID have?
 #'
 #' @return Full or short tag ID as character
 #' @export
 #'
 #' @examples
-#' 
 #' tag = 123
-#' 
 #' atl_full_tag_id(tag)
 #' atl_full_tag_id(tag, short = TRUE)
-#' 
-atl_full_tag_id <- function(tag, short = FALSE){
-  
-  # Make sure tagID has 4 digits
-  x = stringr::str_pad(as.character(tag), 4, pad = "0")
-  
-  # Paste together to get full tag ID
-  y = paste0("3100100", x)
-  
-  # Return full or short tagID as character string
-  ifelse(short == TRUE, return(x), return(y))
-  
+#'
+atl_full_tag_id <- function(tag, short = FALSE, n = 4) {
+
+  # check input
+  assertthat::assert_that(any(is.numeric(tag), is.character(tag)),
+                          msg = "tag provided must be numeric or character")
+  assertthat::assert_that(nchar(as.character(tag)) < 7,
+                          msg = glue::glue("tag should be < 7 digits, \\\n 
+                                           but is {nchar(tag)} digits"))
+
+  # create long tag number
+  tag_full <- as.character(as.numeric(tag) + 31001000000)
+
+  # short format
+  tag_last_n <- substr(tag_full, nchar(tag_full) - n + 1, nchar(tag_full))
+
+  # return full or short tag number as character string
+  ifelse(short == TRUE, return(tag_last_n), return(tag_full))
+
 }
