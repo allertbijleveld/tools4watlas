880f2b8eac8c28d228aceb072643458819d7f4a2
Johannes Krietsch <johannes.krietsch@nioz.nl>
2024-11-28 12:02:28 +0100

    fixed warning for if data are not ordered by time

--- R/fun_get_speed.R
+++ R/fun_get_speed.R
@@ -35,8 +35,8 @@ atl_get_speed <- function(data,
   atl_check_data(data, names_expected = c(x, y, time))
   
   # check whether the data is ordered on time 
-  # if(min(diff(data[, time])) >= 0) 
-  #   warning("data was not ordered by time, but is now")
+  if(min(diff(data[, time])) < 0)
+    warning("data was not ordered by time, but is now")
 
   # set order in time
   data.table::setorderv(data, time)
