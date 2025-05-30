8fb3306898ad2e496afee8e7ec10de4705e9db17
krietsch <johannes.krietsch@nioz.nl>
2025-05-30 11:41:27 +0200

    working on simplifying function

--- R/fun_patch_summary.R
+++ R/fun_patch_summary.R
@@ -29,7 +29,7 @@
 #' @import data.table
 #' @export
 
-atl_patch_summary <- function(data,
+atl_patch_summary_old <- function(data,
                               buffer = 15,
                               summary_variables = c(),
                               summary_functions = c()) {
@@ -132,3 +132,141 @@ atl_patch_summary <- function(data,
   return(data)
 
 }
+
+
+
+
+
+
+
+
+
+
+# load example data
+data <- data_example
+
+# select one individual
+data <- data[tag == 3038]
+
+# Calculate residency patches
+data <- atl_res_patch(
+  data[, .(tag, posID, time, datetime, x, y, speed_in, tideID)],
+  max_speed = 3, lim_spat_indep = 50, lim_time_indep = 180,
+  min_fixes = 3, min_duration = 180
+)
+
+
+
+atl_patch_summary2 <- function(data,
+                              buffer = 15,
+                              summary_variables = c(),
+                              summary_functions = c()) {
+  # Initialize necessary variables to avoid NSE (Non-Standard Evaluation) issues
+  disp_in_patch <- dist_bw_patch <- dist_in_patch <- duration <- NULL
+  nfixes <- patch <- tag <- . <- patchdata <- polygons <- NULL
+  time_end <- time_start <- patch_summary <- NULL
+  x_end <- x_start <- y_end <- y_start <- time_bw_patch <- NULL
+  
+  # Validate input
+  assertthat::assert_that(is.data.frame(data),
+    msg = glue::glue("Input is not a data.frame or data.table, it has class
+                     {stringr::str_flatten(class(data), collapse = ' ')}")
+  )
+  
+  # Check data structure
+  required_columns <- c("tag", "x", "y", "time", "patch")
+  atl_check_data(data, names_expected = required_columns)
+  
+  # Convert data to data.table if not already
+  if (!is.data.table(data)) {
+    data.table::setDT(data)
+  }
+  
+  # Exclude NA
+  d <- data[!is.na(patch)]
+  
+  # Basic summaries
+  ds <- d[, .(
+    nfixes = .N,
+    x_mean = mean(x),
+    x_median = median(x),
+    x_start = first(x),
+    x_end = last(x),
+    y_mean = mean(y),
+    y_median = median(y),
+    y_start = first(y),
+    y_end = last(y),
+    time_mean = mean(time),
+    time_median = median(time),
+    time_start = first(time),
+    time_end = last(time)
+  ), by = .(tag, patch)]
+  
+  # Additional summaries dynamically if any
+  if (length(summary_variables) > 0) {
+    extra_summaries <- d[, lapply(summary_variables, function(var) {
+      lapply(summary_functions, function(fn) {
+        fun <- match.fun(fn)
+        fun(get(var))
+      })
+    }), by = .(tag, patch)]
+    ds <- merge(
+      ds, extra_summaries,
+      by = c("tag", "patch"), all.x = TRUE
+    )
+  }
+  
+  # Distances inside patch
+  dist_in_patch_dt <- d[, .(
+    dist_in_patch = sum(sqrt(diff(x)^2 + diff(y)^2), na.rm = TRUE)
+  ), by = .(tag, patch)]
+  ds[dist_in_patch_dt, on = .(tag, patch), dist_in_patch := i.dist_in_patch]
+  
+  # Distances between patches, time between patches
+  setorder(ds, tag, time_start)
+  ds[, dist_bw_patch := sqrt((x_start - shift(x_end))^2 +
+    (y_start - shift(y_end))^2), by = tag]
+  ds[, time_bw_patch := time_start - shift(time_end), by = tag]
+  
+  # Displacement and duration
+  ds[, disp_in_patch := sqrt((x_end - x_start)^2 + (y_end - y_start)^2)]
+  ds[, duration := time_end - time_start]
+  
+  # Polygons with buffer around points per patch
+  polygons_list <- d[, {
+    pts <- sf::st_as_sf(.SD, coords = c("x", "y"), crs = 32631)
+    buffered <- sf::st_buffer(pts, dist = buffer)
+    list(sf::st_union(buffered))
+  }, by = .(tag, patch)]
+  
+  ds <- merge(ds, polygons_list, by = c("tag", "patch"), all.x = TRUE)
+  setnames(ds, "V1", "polygons")
+  
+  return(ds)
+  
+  
+  
+  
+  # # Check for multipolygons and throw error if any
+  # has_multipolygon <- any(sapply(data$polygons, function(poly) {
+  #   any(st_geometry_type(poly) == "MULTIPOLYGON")
+  # }))
+  # 
+  # if (has_multipolygon) {
+  #   stop("Error: Some polygons are MULTIPOLYGONs, expected only single polygons.")
+  # }
+  
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
